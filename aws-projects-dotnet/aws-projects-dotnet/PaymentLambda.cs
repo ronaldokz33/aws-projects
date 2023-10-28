a6ddfdfdffd1ed2ac7ba4359ae85e475245e5cce
Ronaldo Lemos <ronaldokz_33@hotmail.com>
2023-10-28 15:26:57 +0100

    create lambda dotnet

---
+++ aws-projects-dotnet/aws-projects-dotnet/PaymentLambda.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace aws_projects_dotnet
+{
+    internal class PaymentLambda
+    {
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request)
+        {
+            var id = request.PathParameters["id"];
+
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = JsonConvert.SerializeObject(new
+                {
+                    Ronaldo = "Hello World",
+                    Id = id
+                })
+            };
+        }
+    }
+}
