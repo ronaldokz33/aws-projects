d0ddf6af4328334ec35438a5ac7228598ddccc98
Ronaldo Lemos <ronaldokz_33@hotmail.com>
2023-10-29 16:54:01 +0000

    change function

--- aws-projects-dotnet/lambdas/lambda1/index.js
+++ aws-projects-dotnet/lambdas/lambda1/index.js
@@ -1,6 +1,9 @@
-export async function lambdaHandler(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Hello from Lambda 1' })
+export const handler = async (event) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
     };
-}
+    return response;
+  };
+  
